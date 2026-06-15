2874061f4d8e09b2144120dcfe8e38b03875d018
tx-builder <connected@truex.com>
2026-06-15 16:59:36 +0000

    [TX_BUILDER] published 3.15.3

--- Package.swift
+++ Package.swift
@@ -1,31 +1,26 @@
 // swift-tools-version: 5.10
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
     name: "TruexAdRenderer",
-    platforms:[
+    platforms: [
         .tvOS(.v13)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "TruexAdRenderer",
             targets: ["TruexAdRendererTarget"]),
     ],
     dependencies: [
-        .package(url:"https://github.com/Innovid/tvos-xcframework.git", exact: "1.7.10"),
-        .package(url: "https://github.com/SDWebImage/SDWebImage.git" , exact: "5.19.2"),
+        .package(url: "https://github.com/Innovid/tvos-xcframework.git", .upToNextMinor(from: "1.7.40")),
     ],
     targets: [
         .binaryTarget(name: "TruexAdRenderer",
-                      url: "https://stash.truex.com/integration/TruexAdRenderer-tvOS-v3.15.2-spm.zip",
-                      checksum: "bc51d0054f3980e232c00f0c3cba4489305539904bb3746a50d48efc0ce833b4"),
+                      url: "https://stash.truex.com/integration/TruexAdRenderer-tvOS-v3.15.3-spm.zip",
+                      checksum: "80f0972ffa1f4db71dae3a609b65d6d077b4bca94e47bfc8c5a7505e7a85b067"),
         .target(name: "TruexAdRendererTarget",
                dependencies: [
                 .target(name: "TruexAdRenderer"),
                 .product(name: "InnovidAdRendererFramework", package: "tvos-xcframework"),
-                .product(name: "SDWebImage", package: "SDWebImage"),
                ])
     ]
 )
